2259b05917ae4dcb275f0daf616695f79d11a55e
marianWadia <marianwadia55@gmail.com>
2024-05-29 18:19:24 +0300

    added redux store and cartSlice

---
+++ src/redux/features/cartSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IProduct {
+    id: string;
+    title: string;
+    price: number;
+    img: string;
+    quantity: number;
+}
+
+const initialState : Array<IProduct> = [];
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action: PayloadAction<IProduct>)=>{
+            const itemIndex = state.findIndex((product) => product.id === action.payload.id)
+            if(itemIndex === -1){
+                return [...state, action.payload]
+            }else{
+                return [...state, {...action.payload, quantity: action.payload.quantity + 1}]
+            }
+        },
+        removeFromCart: (state, action:PayloadAction<IProduct>)=>{
+            return state.filter(product => product.id !== action.payload.id)
+        },
+        decreaseQuantity: (state, action: PayloadAction<IProduct>)=>{
+            const itemIndex = state.findIndex((product) => product.id === action.payload.id)
+            if(itemIndex > -1){
+                return [...state, {...action.payload, quantity: action.payload.quantity - 1}]
+            }
+        },
+        clearCart: (state)=>{
+            return []
+        }
+    },
+})
+
+export const { addToCart, removeFromCart, decreaseQuantity, clearCart } = cartSlice.actions
+export default cartSlice.reducer
