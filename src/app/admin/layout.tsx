acaa42888f448dac94e79ac3265dfd5264e87930
marianWadia <marianwadia55@gmail.com>
2024-05-30 18:39:01 +0300

    added in layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,15 +1,23 @@
 "use client"
 import Login from '@/components/adminPanel/login'
+import { useAppSelector } from '@/redux/hooks'
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import Loading from './loading'
 
 export default function Layout() {
     const {data: session} = useSession()
+    const isLoading = useAppSelector(store => store.loadingReducer)
+    
     if(!session?.user){
         return <Login />
     }
 
+
   return (
-    <div>Layout</div>
+    <div>
+      Layout
+     { isLoading && <Loading /> }
+    </div>
   )
 }
